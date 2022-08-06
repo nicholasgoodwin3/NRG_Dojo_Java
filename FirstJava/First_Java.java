c040f0bcf9e5dedf08b0005c7b0b2dec55fa8098
nicholasgoodwin3 <nicholasgoodwin3@gmail.com>
2022-08-05 23:24:22 -0400

    First Edit

---
+++ FirstJava/First_Java.java
@@ -0,0 +1,11 @@
+package FirstJava;
+
+public class First_Java {
+    public static void main(String[] args) {
+        System.out.println("My name is Nicholas Goodwin");
+        System.out.println("I am 29 years old");
+        System.out.println("My hometown is Swansea,SC");
+    }
+}
+
+
